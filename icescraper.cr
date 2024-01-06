3adf067298f595a6ecc61da12e73fea05baa4aba
mephistolist <49227141+mephistolist@users.noreply.github.com>
2024-01-06 02:03:41 +0000

    Update icescraper.cr

--- icescraper.cr
+++ icescraper.cr
@@ -1,4 +1,6 @@
 require "http/client"
+require "option_parser"
+require "colorize"
 
 class Spider
   @start_url : String
@@ -14,16 +16,13 @@ class Spider
   private def crawl_page(url)
     return if @visited_urls.includes?(url)
 
-    # Skip URLs that don't start with http or https
-    return unless url =~ /^https?:\/\//
-
     begin
       response = nil
 
       5.times do
         response = HTTP::Client.get(url)
         
-        break unless response && (300..399).includes?(response.status_code)
+        break unless response && (300..399).includes?(response.status_code) # Don't follow redirects
 
         url = response.headers["Location"] || url
       end
@@ -60,10 +59,64 @@ class Spider
   end
 
   private def extract_links(html)
-    links = html.scan(/href="([^"]+)"/).map { |match| match[0] }
-    links.uniq.map { |link| link.split('"')[1] }
+    html.scan(/href="([^"]+)"/).map { |match| match[0] }.uniq.map { |link| link.split('"')[1] }
   end
 end
 
-spider = Spider.new("https://google.com")
-spider.crawl
+def print_logo
+  logo = " .___                      _________                                               
+|   |  ____   ____       /   _____/  ____ _______ _____   ______    ____ _______  
+|   |_/ ___\\_/ __ \\      \\_____  \\ _/ ___ \\_  __ \\\\__  \\  \\____ \\ _/ __ \\\\_  __ \\ 
+|   |\\  \\___\\  ___/      /        \\\\  \\___ |  | \\/ / __ \\_|  |_> >\\  ___/ |  | \\/ 
+|___| \\___  >\\___  >    /_______  / \\___  >|__|   (____  /|   __/  \\___  >|__|    
+          \\/     \\/             \\/      \\/             \\/ |__|         \\/
+"       
+  puts logo.colorize(:blue)
+end
+
+# Define the code to run after displaying the logo
+def run_after_logo(domain)
+  print_logo
+  scheme = domain.starts_with?("http://") || domain.starts_with?("https://") ? "" : "https://"
+  spider = Spider.new("#{scheme}#{domain}")
+  spider.crawl
+end
+
+# Parse command line options
+option_parser = OptionParser.new do |parser|
+  parser.banner = "Usage: icescraper -d example.com"
+  
+  parser.on("-h", "--help", "Show help message") do
+    print_logo
+    puts parser # Display the usage information
+    exit
+  end
+
+  parser.on("-d", "--domain DOMAIN", "Specify a domain or host") do |domain|
+    run_after_logo(domain)
+    exit
+  end
+
+  parser.on("-o", "--output FILE", "Path to file where urls are saved") do |file|
+    puts "THIS IS THE FILE OPTION: #{file}"
+  end
+end
+
+# Parse the command line arguments
+begin
+  args = option_parser.parse
+rescue
+  print_logo
+  puts option_parser # Display the usage information
+  exit
+end
+
+# Check if a start_url is provided
+if args && args.size == 1
+  start_url = args[0]
+  puts "Start URL specified: #{start_url}"
+else
+  print_logo
+  puts option_parser # Display the usage information
+  exit
+end
